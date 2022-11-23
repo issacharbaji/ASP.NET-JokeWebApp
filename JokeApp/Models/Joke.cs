92a3e0cc2d29b372544ae75af856b76acdd39d34
Issa Charbaji <80466126+issacharbaji@users.noreply.github.com>
2022-11-23 17:33:02 +0200

    Add a model class + controller

---
+++ JokeApp/Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokeApp.Models
+{
+    public class Joke
+    {
+
+        public int ID { get; set; }
+
+        public String JokeQuestion { get; set; }
+        public String JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
